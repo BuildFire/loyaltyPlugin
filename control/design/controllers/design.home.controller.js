dd443d462c53383dd1357a01207de2f075d09dcb
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-11-30 14:46:14 +0530

    Added angular structure for design section

---
+++ control/design/controllers/design.home.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function (angular, window) {
+  angular
+    .module('loyaltyPluginDesign')
+    .controller('DesignHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
+
