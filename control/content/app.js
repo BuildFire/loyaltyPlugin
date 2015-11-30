c3e60aea6fd8272a4455ce206a50d4fadb7eb922
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-11-30 14:10:59 +0530

    Added angular structure for content section

---
+++ control/content/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function (angular) {
+  angular.module('loyaltyPluginContent', ['ngRoute','ui.bootstrap'])
+    //injected ngRoute for routing
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'templates/home.html',
+          controllerAs: 'ContentHome',
+          controller: 'ContentHomeCtrl'
+        })
+        .otherwise('/');
+    }])
+})(window.angular);
---
+++ control/content/controllers/content.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular) {
+  angular
+    .module('loyaltyPluginContent')
+    .controller('ContentHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
