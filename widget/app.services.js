fadb9648056b3a24e6695906ad7f64bd72c0fe9d
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-12-01 21:39:57 +0530

    Added enums and app.services.js for widget section

---
+++ widget/app.services.js
@@ -0,0 +1,187 @@
+'use strict';
+
+(function (angular, buildfire) {
+  angular.module('loyaltyPluginWidget')
+    .provider('Buildfire', [function () {
+      var Buildfire = this;
+      Buildfire.$get = function () {
+        return buildfire
+      };
+      return Buildfire;
+    }])
+    .factory('Location', [function () {
+      var _location = window.location;
+      return {
+        goTo: function (path) {
+          _location.href = path;
+        }
+      };
+    }])
+    .factory('ViewStack', ['$rootScope', function ($rootScope) {
+      var views = [];
+      var viewMap = {};
+      return {
+        push: function (view) {
+          if (viewMap[view.template]) {
+            this.pop();
+          }
+          else {
+            viewMap[view.template] = 1;
+            views.push(view);
+            $rootScope.$broadcast('VIEW_CHANGED', 'PUSH', view);
+          }
+          return view;
+        },
+        pop: function () {
+          $rootScope.$broadcast('BEFORE_POP', views[views.length - 1]);
+          var view = views.pop();
+          delete viewMap[view.template];
+          $rootScope.$broadcast('VIEW_CHANGED', 'POP', view);
+          return view;
+        },
+        hasViews: function () {
+          return !!views.length;
+        },
+        getCurrentView: function () {
+          return views.length && views[views.length - 1] || {};
+        },
+        popAllViews: function () {
+          $rootScope.$broadcast('VIEW_CHANGED', 'POPALL', views);
+          views = [];
+        }
+      };
+    }])
+    .factory('LoyaltyAPI', ['$q', 'STATUS_CODE', 'STATUS_MESSAGES', 'SERVER', '$http',
+      function ($q, STATUS_CODE, STATUS_MESSAGES, SERVER, $http) {
+        var addApplication = function (app) {
+          var deferred = $q.defer();
+          if (!app) {
+            deferred.reject(new Error('Undefined app data'));
+          }
+          $http.post(SERVER.URL + '/api/loyaltyApp', {
+            data: app
+          }).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+        var getApplication = function (id) {
+          var deferred = $q.defer();
+          if (!id) {
+            deferred.reject(new Error('Undefined app id'));
+          }
+          $http.get(SERVER.URL + '/api/loyaltyApp/' + id).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+
+        var addReward = function (data) {
+          var deferred = $q.defer();
+          if (!data) {
+            deferred.reject(new Error('Undefined reward data'));
+          }
+          $http.post(SERVER.URL + '/api/loyaltyRewards', {data: data}).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+
+        var getRewards = function (id) {
+          var deferred = $q.defer();
+          if (!id) {
+            deferred.reject(new Error('Undefined app id'));
+          }
+          $http.get(SERVER.URL + '/api/loyaltyRewards/' + id).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+
+        var updateReward = function (data) {
+          var deferred = $q.defer();
+          if (!data._id) {
+            deferred.reject(new Error('Undefined reward id'));
+          }
+          $http.post(SERVER.URL + '/api/loyaltyRewards', {data: data}).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+
+        var removeReward = function (id) {
+          var deferred = $q.defer();
+          if (!id) {
+            deferred.reject(new Error('Undefined reward id'));
+          }
+          $http.delete(SERVER.URL + '/api/loyaltyRewards/' + id).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+
+        var sortRewards = function (data) {
+          var deferred = $q.defer();
+          if (!data.appId) {
+            deferred.reject(new Error('Undefined app Id'));
+          }
+          $http.post(SERVER.URL + '/api/loyaltyRewardsSort', {data: data}).success(function (response) {
+            if (response.statusCode == 200)
+              deferred.resolve(response);
+            else
+              deferred.resolve(null);
+          })
+            .error(function (error) {
+              deferred.reject(error);
+            });
+          return deferred.promise;
+        };
+
+
+        return {
+          addApplication: addApplication,
+          getApplication: getApplication,
+          addReward: addReward,
+          getRewards: getRewards,
+          updateReward: updateReward,
+          removeReward: removeReward,
+          sortRewards : sortRewards
+        };
+      }])
+})(window.angular, window.buildfire);
