d121e7c7cb430532ef4eb2af382d98c111e6e09b
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-12-07 17:11:08 +0530

    Formatted and added comments in widget item controller

--- widget/controllers/widget.item.controller.js
+++ widget/controllers/widget.item.controller.js
@@ -7,12 +7,22 @@
       function ($scope, ViewStack, RewardCache, $sce) {
 
         var WidgetItem = this;
+
+        /**
+         * Initialize variable with current view returned by ViewStack service. In this case it is "Item_Details" view.
+         */
         var currentView = ViewStack.getCurrentView();
 
+        /**
+         * Initialize WidgetItem.reward with reward details set in home controller
+         */
         if (RewardCache.getReward()) {
           WidgetItem.reward = RewardCache.getReward();
         }
 
+        /**
+         * Check if user's total loyalty points are enough to redeem the reward, if yes redirect to next page.
+         */
         WidgetItem.confirmCancel = function () {
           if (WidgetItem.reward.pointsToRedeem <= currentView.totalPoints) {
             ViewStack.push({
@@ -23,6 +33,9 @@
           }
         };
 
+        /**
+         * To parse and show reward's description in html format
+         */
         WidgetItem.safeHtml = function (html) {
           if (html)
             return $sce.trustAsHtml(html);
