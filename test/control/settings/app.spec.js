629924a28dcd576a1355b64151eeddcc83389848
tabish.khan <tabish.khan@tothenew.com>
2015-12-14 18:32:41 +0530

    Added setting controller test cases for setting section.

---
+++ test/control/settings/app.spec.js
@@ -0,0 +1,23 @@
+describe('Unit: loyaltyPluginSettings design app', function () {
+  describe('Unit: app routes', function () {
+    beforeEach(module('loyaltyPluginSettings'));
+    var route, rootScope;
+    beforeEach(inject(function ( _$route_, _$rootScope_) {
+      route = _$route_;
+      rootScope = _$rootScope_;
+    }));
+    describe('Home route', function () {
+      beforeEach(inject(
+        function ($httpBackend) {
+          $httpBackend.expectGET('templates/home.html')
+            .respond(200);
+          $httpBackend.expectGET('/')
+            .respond(200);
+        }));
+
+      it('should load the home page for design on successful load of location path /', function () {
+
+      });
+    });
+  });
+});
