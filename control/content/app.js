f11c01f89ef0c2c8cc74aa981f11086da4ccd355
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-04-07 23:23:02 +0530

    Added new placeholder image in content section as well

--- control/content/app.js
+++ control/content/app.js
@@ -1,76 +1,91 @@
 'use strict';
 
 (function (angular, buildfire) {
-  angular.module('loyaltyPluginContent', ['ngRoute', 'ui.bootstrap', 'ui.tinymce',
-    'ui.sortable', 'ngAnimate'])
-    //injected ngRoute for routing
-    .config(['$routeProvider', function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'templates/home.html',
-          controllerAs: 'ContentHome',
-          controller: 'ContentHomeCtrl',
-          resolve: {
-            context: function (Context) {
-              return Context.getContext();
+    angular.module('loyaltyPluginContent', ['ngRoute', 'ui.bootstrap', 'ui.tinymce',
+        'ui.sortable', 'ngAnimate'])
+        //injected ngRoute for routing
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'templates/home.html',
+                    controllerAs: 'ContentHome',
+                    controller: 'ContentHomeCtrl',
+                    resolve: {
+                        context: function (Context) {
+                            return Context.getContext();
+                        }
+                    }
+                })
+                .when('/reward', {
+                    templateUrl: 'templates/reward.html',
+                    controllerAs: 'ContentReward',
+                    controller: 'ContentRewardCtrl',
+                    resolve: {
+                        context: function (Context) {
+                            return Context.getContext();
+                        }
+                    }
+                })
+                .when('/reward/:id', {
+                    templateUrl: 'templates/reward.html',
+                    controllerAs: 'ContentReward',
+                    controller: 'ContentRewardCtrl',
+                    resolve: {
+                        context: function (Context) {
+                            return Context.getContext();
+                        }
+                    }
+                })
+                .otherwise('/');
+        }]).filter('getImageUrl', function () {
+            return function (url, width, height, type) {
+                if (type == 'resize')
+                    return buildfire.imageLib.resizeImage(url, {
+                        width: width,
+                        height: height
+                    });
+                else
+                    return buildfire.imageLib.cropImage(url, {
+                        width: width,
+                        height: height
+                    });
             }
-          }
         })
-        .when('/reward', {
-          templateUrl: 'templates/reward.html',
-          controllerAs: 'ContentReward',
-          controller: 'ContentRewardCtrl',
-          resolve: {
-            context: function (Context) {
-              return Context.getContext();
-            }
-          }
-        })
-        .when('/reward/:id', {
-          templateUrl: 'templates/reward.html',
-          controllerAs: 'ContentReward',
-          controller: 'ContentRewardCtrl',
-          resolve: {
-            context: function (Context) {
-              return Context.getContext();
-            }
-          }
-        })
-        .otherwise('/');
-    }]).filter('getImageUrl', function () {
-      return function (url, width, height, type) {
-        if (type == 'resize')
-          return buildfire.imageLib.resizeImage(url, {
-            width: width,
-            height: height
-          });
-        else
-          return buildfire.imageLib.cropImage(url, {
-            width: width,
-            height: height
-          });
-      }
-    })
-    .run(['$location', '$rootScope', 'RewardCache', function ($location, $rootScope, RewardCache) {
-      buildfire.messaging.onReceivedMessage = function (msg) {
-        switch (msg.type) {
-          case 'OpenItem':
-            RewardCache.setReward(msg.data);
-            $location.path('/reward/' + msg.data._id);
-            $rootScope.$apply();
-            break;
-          case 'BackToHome':
-            buildfire.history.pop();
-            $location.path('/');
-            $rootScope.$apply();
-            break;
-        }
-      };
-        buildfire.history.onPop(function(data, err){
-          if(data.label !=  "Reward" ){
-            $location.path('/');
-            $rootScope.$apply();
-          }
-        })
-    }])
+        .directive("loadImage", [function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
+
+                    var elem = $("<img>");
+                    elem[0].onload = function () {
+                        element.attr("src", attrs.finalSrc);
+                        elem.remove();
+                    };
+                    elem.attr("src", attrs.finalSrc);
+                }
+            };
+        }])
+        .run(['$location', '$rootScope', 'RewardCache', function ($location, $rootScope, RewardCache) {
+            buildfire.messaging.onReceivedMessage = function (msg) {
+                switch (msg.type) {
+                    case 'OpenItem':
+                        RewardCache.setReward(msg.data);
+                        $location.path('/reward/' + msg.data._id);
+                        $rootScope.$apply();
+                        break;
+                    case 'BackToHome':
+                        buildfire.history.pop();
+                        $location.path('/');
+                        $rootScope.$apply();
+                        break;
+                }
+            };
+            buildfire.history.onPop(function (data, err) {
+                if (data.label != "Reward") {
+                    $location.path('/');
+                    $rootScope.$apply();
+                }
+            })
+        }])
 })(window.angular, window.buildfire);
